410c29c71bba82624cab73d4965a845c874909ac
AnthonySosa <tony96sosa@hotmail.com>
2020-08-08 14:14:31 -0300

    practica de JavaScript y CSS

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,21 @@
-let colors = ['#C0392B','#884EA0','#7D3C98','#2471A3 ','#17A589 ',
-'#D4AC0D','#BA4A00'];
-let i=0;
-const button = document.querySelector("button");
-const cambiarColor = () => {
-    if(i<colors.length){
-        i++;
-    }else{
-        i=0;
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const button = document.querySelector(".button");
+const color = document.querySelector(".color");
+
+const getRandomInt = () => { 
+    return Math.floor(Math.random() * hex.length); 
+} 
+const getColor = () => {
+    let colorHex = '#';
+    for(let i=0; i < 6; i++){
+        colorHex += hex[getRandomInt()];
     }
+    return colorHex;
+}
+const cambiarColor = () => {
+    let colorHex = getColor();
     const body = document.querySelector("body");
-    body.style.background = colors[i];
+    body.style.background = colorHex;
+    color.textContent = colorHex;
 }
 button.addEventListener('click', cambiarColor );
